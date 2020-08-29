11623922a44303edb49d3ef0cffc8ca5de535108
hoangle <hoangle20590@gmail.com>
2020-08-28 21:46:21 -0500

    main.py now cuts a video into chunks of 3 quality

--- main.py
+++ main.py
@@ -1,41 +1,59 @@
-import subprocess, math
+import os, subprocess, math
 
 
+# create folder to store chunks; folder same name as file
+"""
+parent_dir = "C:/Users/Hoang/Documents/project1/SunnyNStreamServer"
+# get video name to src         then        src = 'videos/' + src       OR
+src = 'videos/test2.mp4'
+dir = src.replace('.mp4', '')
 
-cmd = 'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 videos/test.mp4'
+path = os.path.join(parent_dir, dir)            
+try: 
+    os.mkdir(path) 
+except OSError as error: 
+    print(error)
+"""
+
+
+src = 'videos/test2.mp4'
+cmd = 'ffprobe -v error -show_entries format=duration -of default=nw=1:nk=1 ' + src
 output = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
 
-len = float(output.stdout)
-len = int(len)
-
+len = math.ceil(float(output.stdout))
 chunks = math.ceil((len/5))
-
-src = 'videos/test.mp4'
-# subprocess.run("mkdir videos/test", shell=True) # not working
-
 
 # ffmpeg -i videos/test.mp4 -vf scale=640:480 -ss 0 -t 5 -strict -2 videos/test/480_00000.mp4
 # ffmpeg -i videos/test.mp4 -vf scale=1280:720 -ss 0 -t 5 -strict -2 videos/test/720_00000.mp4
 # ffmpeg -i videos/test.mp4 -vf scale=1280:1024 -ss 0 -t 5 -strict -2 videos/test/1080_00000.mp4
 
-video = '  ffmpeg -i "src" -ss start -t 5 -strict -2 "des" '
+video = '  ffmpeg -i "src" -vf scale=640:480 -ss start -t 5 -strict -2 "des" ' # aac is experimental, use -strict -2 BEFORE the last argument
 
 for i in range(chunks):
-
-# modify cmd
-    num = str(i).zfill(5) # file name suffix
-    des = src.replace('.mp4', '/480_' + num + '.mp4') # modify destination files
-    cmd = video.replace('src', src)  # get the source file
-    cmd = cmd.replace('start', str(i*5))# get the starting position for cutting
-    cmd = cmd.replace('des', des)  # get the destination file
+    
+    # modify cmd    
+    num = str(i).zfill(5)                               # file name suffix    
+    cmd = video.replace('src', src)                     # source file path
+    cmd = cmd.replace('start', str(i*5))                # starting position for cutting
+    des = src.replace('.mp4', '/480_' + num + '.mp4')   # destination file name
+    cmd = cmd.replace('des', des)                       # destination file path    
     subprocess.run(cmd, shell=True)
     
-    cmd = cmd.replace('480', '720')  # get the destination file
+    cmd = cmd.replace('640:480', '1024:720')            # resolution
+    cmd = cmd.replace('480_', '720_')                   # destination file name
     subprocess.run(cmd, shell=True)
 
-    cmd = cmd.replace('720', '1080')  # get the destination file
+    cmd = cmd.replace('1024:720', '1280:1024')    
+    cmd = cmd.replace('720_', '1024_')
     subprocess.run(cmd, shell=True)
+
+
+
+
+
+
+
 
 
 
@@ -50,19 +68,18 @@ for i in range(chunks):
     cmd = cmd.replace('des', des)  # get the destination file
     subprocess.run(cmd, shell=True)
 
-cmd = cmd.replace('480', '720')  # get the destination file
-subprocess.run(cmd, shell=True)
+    cmd = cmd.replace('480', '720')  # get the destination file
+    subprocess.run(cmd, shell=True)
 
-cmd = cmd.replace('720', '1080')  # get the destination file
-subprocess.run(cmd, shell=True)
+    cmd = cmd.replace('720', '1080')  # get the destination file
+    subprocess.run(cmd, shell=True)
 """
 
 
 
 
-
-"""
 # not working 
+"""
 
 src = 'videos/test.mp4'
 f = open(src, 'r')
