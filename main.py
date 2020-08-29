76b683599e69eefb7e82ec9ab4c33793cf2d226d
hoangle <hoangle20590@gmail.com>
2020-08-29 14:37:34 -0500

    close #1

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
-import os, subprocess, math
+import os, subprocess, math, argparse, ffmpeg
 
 
-src = 'videos/test2.mp4'
-cmd = 'ffprobe -v error -show_entries format=duration -of default=nw=1:nk=1 ' + src
-output = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+# How many chunks?
+probe = ffmpeg.probe('videos/test.mp4')
+info = next(stream for stream in probe['streams'] if stream['codec_type'] == 'video')
+duration = math.ceil(float(info['duration']))
+chunks = math.ceil(duration/5)
 
 
-len = math.ceil(float(output.stdout))
-chunks = math.ceil((len/5))
+for i in range (chunks):    
+    num = str(i).zfill(5)
+    des = "videos/test/480_"  + num + '.mp4'
 
-# ffmpeg -i videos/test.mp4 -vf scale=640:480 -ss 0 -t 5 -strict -2 videos/test/480_00000.mp4
-# ffmpeg -i videos/test.mp4 -vf scale=1280:720 -ss 0 -t 5 -strict -2 videos/test/720_00000.mp4
-# ffmpeg -i videos/test.mp4 -vf scale=1280:1024 -ss 0 -t 5 -strict -2 videos/test/1080_00000.mp4
-
-video = '  ffmpeg -i "src" -vf scale=640:480 -ss start -t 5 -strict -2 "des" ' # aac is experimental, use -strict -2 BEFORE the last argument
-
-for i in range(chunks):
+    input = ffmpeg.input('videos/test.mp4', ss=i*5, t=5)
+    audio = input.audio.filter('atrim')
+    vid1 = input.video.filter('scale', '640', '480')        # -1:480 doesn't work, but 480:-1 does
+    vid2 = input.video.filter('scale', '1280', '720')
+    vid3 = input.video.filter('scale', '1280', '1024')      # 1920:1080 cause malloc error
     
-    # modify cmd    
-    num = str(i).zfill(5)                               # file name suffix    
-    cmd = video.replace('src', src)                     # source file path
-    cmd = cmd.replace('start', str(i*5))                # starting position for cutting
-    des = src.replace('.mp4', '/480_' + num + '.mp4')   # destination file name
-    cmd = cmd.replace('des', des)                       # destination file path    
-    subprocess.run(cmd, shell=True)
     
-    cmd = cmd.replace('640:480', '1024:720')            # resolution
-    cmd = cmd.replace('480_', '720_')                   # destination file name
-    subprocess.run(cmd, shell=True)
-
-    cmd = cmd.replace('1024:720', '1280:1024')    
-    cmd = cmd.replace('720_', '1024_')
-    subprocess.run(cmd, shell=True)
+    ffmpeg.output(audio, vid1, des, strict='-2').run()
+    des = des.replace('480_', '720_')
+    ffmpeg.output(audio, vid2, des, strict='-2').run()
+    des = des.replace('720_', '1024_')
+    ffmpeg.output(audio, vid3, des, strict='-2').run()
