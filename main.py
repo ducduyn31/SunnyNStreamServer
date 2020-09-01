329316153dc1a0ddf05c0d6eaec4f7f758594918
hoangle <hoangle20590@gmail.com>
2020-09-01 17:13:29 -0500

    added argparse functionality

--- main.py
+++ main.py
@@ -1,26 +1,95 @@
-import os, subprocess, math, argparse, ffmpeg
+import math, argparse, ffmpeg
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Cut videos')
+parser.add_argument('-src', '--source', type=str, metavar='', required=True, dest='src', help='file path')
+args = parser.parse_args()
+
+def cut_vid(src):
+    # How many chunks?
+    probe = ffmpeg.probe(src)
+    info = next(stream for stream in probe['streams'] if stream['codec_type'] == 'video')
+    duration = math.ceil(float(info['duration']))
+    chunks = math.ceil(duration/5)
+    clen = 1        # chunk length
+    
+
+    for i in range (chunks):    
+                
+        input = ffmpeg.input('videos/test.mp4', ss=i*clen, t=clen)
+        audio = input.audio.filter('atrim')
+        
+        num = str(i).zfill(5)        
+        h = '360'
+        des = src.replace('.mp4', '/' + h + '_'  + num + '.mp4')        
+        vid = input.video.filter('scale', '-2', h)        
+        ffmpeg.output(audio, vid, des, strict='experimental').run()
+
+        h = '480'
+        des = src.replace('.mp4', '/' + h + '_'  + num + '.mp4')
+        vid = input.video.filter('scale', '-2', h)    
+        ffmpeg.output(audio, vid, des, strict='experimental').run()
+        
+        h = '720'
+        des = src.replace('.mp4', '/' + h + '_'  + num + '.mp4')
+        vid = input.video.filter('scale', '-2', h) 
+        ffmpeg.output(audio, vid, des, strict='experimental').run()
+    
+
+if __name__ == '__main__':
+    cut_vid(args.src)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# backup, working program as of 2020 sep 01
+"""
+src = input('Please enter the file path: ')
 # How many chunks?
-probe = ffmpeg.probe('videos/test.mp4')
+probe = ffmpeg.probe(src)
 info = next(stream for stream in probe['streams'] if stream['codec_type'] == 'video')
 duration = math.ceil(float(info['duration']))
 chunks = math.ceil(duration/5)
+clen = 1        # chunk length
 
 
 for i in range (chunks):    
     num = str(i).zfill(5)
-    des = "videos/test/480_"  + num + '.mp4'
+    des = src.replace('.mp4', '/' + h + '_'  + num + '.mp4')
+    #des = "videos/test/" + h + '_'  + num + '.mp4'
 
-    input = ffmpeg.input('videos/test.mp4', ss=i*5, t=5)
+    input = ffmpeg.input('videos/test.mp4', ss=i*clen, t=clen)
     audio = input.audio.filter('atrim')
-    vid1 = input.video.filter('scale', '640', '480')        # -1:480 doesn't work, but 480:-1 does
-    vid2 = input.video.filter('scale', '1280', '720')
-    vid3 = input.video.filter('scale', '1280', '1024')      # 1920:1080 cause malloc error
+    vid = input.video.filter('scale', '-2', '360')
     
-    
-    ffmpeg.output(audio, vid1, des, strict='-2').run()
-    des = des.replace('480_', '720_')
-    ffmpeg.output(audio, vid2, des, strict='-2').run()
-    des = des.replace('720_', '1024_')
-    ffmpeg.output(audio, vid3, des, strict='-2').run()
+    ffmpeg.output(audio, vid, des, strict='experimental').run()
+
+    # h = '480'
+    # vid = input.video.filter('scale', '-2', h)    
+    # ffmpeg.output(audio, vid, des, strict='experimental').run()
+    # h = '720'
+    # vid = input.video.filter('scale', '-2', h) 
+    # ffmpeg.output(audio, vid, des, strict='experimental').run()
+"""
+
+
